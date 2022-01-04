601fdfaee4dd51d9e9c69da507d44a78b25ab7ec
alejandrolima-cbqa <alejandro.lima@cbqasolutions.com>
2022-01-03 20:59:17 -0600

    testimonials-form

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -21,13 +21,40 @@
             <title>Dashboard Admin</title>
         </head>
         <body>
-            <div class="container">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="container text-center" style="margin-top: 100px;">
+                <div class="row">
+                    <div class="col-md">
+                        <div class="card" style="width: 30rem;">
+                            <img src="../assets/images/emLogo.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Testimonios</h5>
+                                <p class="card-text">Sube un testimonio que contenga: 1 imagen, 1 video, 1 breve descripcion, 1 frase relevante, nombre, oficio y tipo de cultivo.</p>
+                                <br>
+                                <a href="upTestimonials.php" class="btn btn-primary">Subir testimonio</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <!-- <div class="card" style="width: 18rem;">
+                            <img src="../assets/images/emLogo.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <br>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div> -->
+                    </div>
+                    <div class="col-md">
+                        <div class="card" style="width: 30rem;">
+                            <img src="../assets/images/emLogo.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Productos</h5>
+                                <p class="card-text">Sube un producto que contenga titulo, descripcion, lista de contenido (separada por comas), foto, tipo de producto, presentaciones (separadas por comas), beneficios y pdf</p>
+                                <br>
+                                <a href="#" class="btn btn-primary">Subir producto</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ pages/upTestimonials.php
@@ -0,0 +1,75 @@
+<?php
+
+    include('conection.php');
+    include('funciones.php');
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/fontawesome-all.min.css" rel="stylesheet">
+    <link href="../css/aos.min.css" rel="stylesheet">
+    <link href="../css/swiper.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <script type="text/javascript" src="../js/main.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="mb-3">
+        <br>
+        <h4 class="text-center">Llena los datos del testimonio</h4>
+            <form method='post' enctype='multipart/form-data' action='./modificacion.php?padre=3&accion=1' name='principal'>
+                <div class="col-md-4">
+                    <label for="validationDefault01" class="form-label" style="color:black;">Nombre del usuario</label>
+                    <input type="text" class="form-control" id="validationDefault01" name="name" placeholder="Nombre" required>
+                </div>
+                <div class="col-md-4">
+                    <label for="validationDefault02" class="form-label" style="color:black;">A que se dedica:</label>
+                    <?php
+                        LlenaComboSaltado("select IdActividad, Descripcion from tipoactividad order by IdActividad","algo","actividad");
+                    ?>
+                </div>
+                <div class="col-md-4">
+                    <label for="validationDefault02" class="form-label" style="color:black;">Tipo de cultivo que tiene</label>
+                    <?php
+                        LlenaComboSaltado("select IdTipo, Descripcion from tipocultivo order by IdTipo","algo","cultivo");
+                    ?>
+                </div>
+                <div class="col-md-4">
+                    <label for="validationDefault02" class="form-label" style="color:black;">Descripcion breve del testimonio</label>
+                    <input type="text" class="form-control" id="validationDefault02" name="des" placeholder="Un campesino ..." required>
+                </div>
+                <div class="col-md-4">
+                    <label for="validationDefault02" class="form-label" style="color:black;">Frase relevante del testimonio</label>
+                    <input type="text" class="form-control" id="validationDefault02" name="frase" placeholder="'El mejor producto...'" required>
+                </div>
+                <div class="col-md-4">
+                    <label for="validationDefault02" class="form-label" style="color:black;">Imagen</label>
+                    <input type="file" class="form-control" id="validationDefault02" name="im1" required>
+                </div>
+                <div class="col-md-4">
+                    <label for="validationDefault02" class="form-label" style="color:black;">Video</label>
+                    <input type="file" class="form-control" id="validationDefault02" name="vi1" required>
+                </div>
+                <br>
+                <button class='btn btn-outline-dark btnBajo' type='submit' href='./modificacion.php?padre=3&accion=1'>Registrar</button>
+            </form>
+        </div>
+    </div>
+    <!-- Scripts -->
+    <script src="../js/bootstrap.min.js"></script><!-- Bootstrap framework -->
+    <script src="../js/purecounter.min.js"></script> <!-- Purecounter counter for statistics numbers -->
+    <script src="../js/swiper.min.js"></script><!-- Swiper for image and text sliders -->
+    <script src="../js/aos.js"></script><!-- AOS on Animation Scroll -->
+    <script src="../js/script.js"></script>  <!-- Custom scripts -->
+</body>
+</html>
